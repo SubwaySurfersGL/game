6dc7f97b9cca93cd34990f2ffda90dda1007feae
ehotigas <twsandrade@gmail.com>
2023-12-04 17:56:37 -0300

    comment

--- source/colisao.c
+++ source/colisao.c
@@ -1,14 +1,15 @@
 #include "personagens.h"
 
 int checkCollision() {
-    float personagemPosX = -44;
-    float personagemPosY = -44;
-    float personagemPosZ = 1.0;
-    int collisionX = one.Position.x + one.Size.x >= two.Position.x &&
-        two.Position.x + two.Size.x >= one.Position.x;
+    // personagemPosX = -44;
+    // personagemPosY = -44;
+    // personagemPosZ = 1.0;
+    // int collisionX = one.Position.x + one.Size.x >= two.Position.x &&
+    //     two.Position.x + two.Size.x >= one.Position.x;
 
-    int collisionY = one.Position.y + one.Size.y >= two.Position.y &&
-        two.Position.y + two.Size.y >= one.Position.y;
+    // int collisionY = one.Position.y + one.Size.y >= two.Position.y &&
+    //     two.Position.y + two.Size.y >= one.Position.y;
 
-    return collisionX && collisionY;
+    // return collisionX && collisionY;
+    return 0;
 }  
